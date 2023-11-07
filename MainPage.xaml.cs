e419b02c23d783c272db33f273fe61083951c08a
CORREA <betsabe.correa@tecnologicoliceocristiano.edu.ec>
2023-11-06 21:29:59 -0500

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace RegistroEstudiante
+{
+    public partial class MainPage : ContentPage
+    {
+        public Estudiante MiEstudiante { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            MiEstudiante = new Estudiante();
+
+            BindingContext = MiEstudiante;
+
+        }
+
+        private void ConfirmarBtn_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Registro",
+                MiEstudiante.Nombres +
+                " sus datos se ingresaron correctamente.", "Ok");
+
+        }
+    }
+}
