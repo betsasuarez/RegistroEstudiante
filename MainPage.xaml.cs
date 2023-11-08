5e2f41e61b5735a966ebe17cf9a9a1b452e30d1d
CORREA <betsabe.correa@tecnologicoliceocristiano.edu.ec>
2023-11-07 20:57:34 -0500

    IsValid

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,6 +2,7 @@
 {
     public partial class MainPage : ContentPage
     {
+         private  string titulo = "Registro";
         public Estudiante MiEstudiante { get; set; }
 
         public MainPage()
@@ -15,9 +16,35 @@
 
         private void ConfirmarBtn_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Registro",
-                MiEstudiante.Nombres +
-                " sus datos se ingresaron correctamente.", "Ok");
+            // DisplayAlert("Registro", MiEstudiante.Nombres +
+            //    " sus datos se ingresaron correctamente.", "Ok");
+
+            if (IsValid() ==true)
+            {
+                var mensaje = $"{MiEstudiante.Nombres} sus datos se ingresaron correctamente";
+                string titulo = "Registro";
+                DisplayAlert(titulo, mensaje, "ok");
+            }
+        }
+         private bool IsValid()
+        {
+            
+            if (string.IsNullOrEmpty (MiEstudiante.Nombres)) 
+            { 
+                DisplayAlert(titulo, "Debe Ingresar los Nombres", "ok");
+                    return false;
+            }
+            if (string.IsNullOrEmpty(MiEstudiante.Apellidos))
+            {
+                DisplayAlert(titulo, "Debe Ingresar los Apellidos", "ok");
+                return false;
+            }
+            if (string.IsNullOrEmpty(MiEstudiante.Materia))
+            {
+                DisplayAlert(titulo, "Debe Ingresar la Materia", "ok");
+                return false;
+            }
+            return true;
 
         }
     }
